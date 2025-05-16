9c70baa9f9ce513dda5a54211887b1c79ec3a619
Eshwar Bachu <bachueshwar46@gmail.com>
2025-05-16 16:45:26 +0530

    Initial commit: Add client and server code

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>🌍 DisasterScope</h1>
+      <p>Real-time disaster detection dashboard</p>
+    </div>
+  );
+}
+
+export default App;
